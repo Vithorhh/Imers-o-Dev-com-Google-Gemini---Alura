4383c7b51bce4d23160a4986f0308779452a2cc2
Vithorhh <106975119+Vithorhh@users.noreply.github.com>
2024-09-05 08:26:09 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Fairy Tail",
+        descricao: "Fairy Tail é um anime que acompanha as aventuras de Natsu Dragneel, um jovem mago que busca seu pai adotivo, o dragão Igneel, junto com sua guilda, a Fairy Tail.",
+        link: "https://pt.wikipedia.org/wiki/Fairy_Tail"
+    },
+    {
+        titulo: "Naruto",
+        descricao: "Naruto é um ninja que busca o reconhecimento de sua vila e sonha em se tornar Hokage. A história acompanha sua jornada e seus laços de amizade.",
+        link: "https://pt.wikipedia.org/wiki/Naruto"
+    },
+    {
+        titulo: "One Piece",
+        descricao: "One Piece é uma aventura épica que acompanha Monkey D. Luffy em sua busca pelo tesouro mais procurado do mundo, o One Piece, para se tornar o Rei dos Piratas.",
+        link: "https://pt.wikipedia.org/wiki/One_Piece"
+    }
+];
+
